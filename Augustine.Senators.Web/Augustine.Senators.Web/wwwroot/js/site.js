77b13aab9511354eb8ff6c323088bd8bb62d1550
Casey Kriutzfield <casey.kriutzfield@live.com>
2019-02-06 12:41:43 -0500

    Created a page to list senators using dummy data.

--- Augustine.Senators.Web/Augustine.Senators.Web/wwwroot/js/site.js
+++ Augustine.Senators.Web/Augustine.Senators.Web/wwwroot/js/site.js
@@ -2,9 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+// Sample Model for a Senator
+var senatorsList = [
+    { "Id": 0, "Name": "Spongebob Squarepants", "District": 2, "Party": "Democrat", "PhoneNumber": "555-369-2101", "EmailAddress": "sbob@gmail.com" },
+    { "Id": 1, "Name": "Patrick Star", "District": 11, "Party": "Democrat", "PhoneNumber": "555-326-9111", "EmailAddress": "pstar@hotmail.com" }
+];
+
+var getSenators = function () {
+    //var sens = JSON.parse(senatorsList);
+    return senatorsList;
+};
+
 var indexApp = new Vue({
     el: '#index',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        senators: getSenators().length
+    }
+});
+
+var senatorApp = new Vue({
+    el: '#senatorMain',
+    data: {
+        message: 'Hello Vue!',
+        senators: getSenators()
     }
 })
