9cab6d1b6dfacd68e5615b85594d554e8607a588
Kevin Kraus <kevin.kraus@outlook.com>
2019-03-04 20:41:51 -0700

    added IdentityServer4 microservice for using during our demo

---
+++ Augustine.Identity.Microservice/Augustine.IDP/Config.cs
@@ -0,0 +1,77 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Augustine.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "CC05868E-DA05-4395-9160-437AEEE208D0",
+                    Username = "kkraus",
+                    Password = "pass@word1",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Kevin"),
+                        new Claim("family_name", "Kraus"),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "82C00D20-C3F7-41E8-A1D1-93AB88D6C1BE",
+                    Username = "ckriutz",
+                    Password = "pass@word1",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Casey"),
+                        new Claim("family_name", "Kriutzfield"),
+                    }
+                }
+            };
+        }
+
+        internal static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientName = "Augustine.Senator.Web",
+                    ClientId = "augustinesenatorclient",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:8888/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+    }
+}
