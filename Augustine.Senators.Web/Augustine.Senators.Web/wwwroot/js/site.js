3494e8403456309107dce4c569e24b0d2038d4e1
Casey Kriutzfield <casey.kriutzfield@live.com>
2019-03-15 12:49:53 -0400

    Updates to the Web Project to do crud operations using vue.js.

--- Augustine.Senators.Web/Augustine.Senators.Web/wwwroot/js/site.js
+++ Augustine.Senators.Web/Augustine.Senators.Web/wwwroot/js/site.js
@@ -9,31 +9,86 @@ var senatorsList = [
     { "Id": 1, "Name": "Patrick Star", "District": 11, "Party": "Democrat", "PhoneNumber": "555-326-9111", "EmailAddress": "pstar@hotmail.com" }
 ];
 
+// This function will reach out to the service and get all the senators.
 var getSenators = function () {
-    //var sens = JSON.parse(senatorsList);
+    //senatorsList = 
     return senatorsList;
 };
 
 var indexApp = new Vue({
     el: '#index',
     data: {
-        message: 'Hello Vue!',
         senators: getSenators().length
     }
 });
+
 
 var senatorApp = new Vue({
     el: '#senatorMain',
     data: {
         message: 'Hello Vue!',
-        senators: getSenators()
+        senators: getSenators(),
+        SenatorToEdit: '',
+        SenatorToAdd: [
+            {
+                Id: '',
+                Name: '',
+                District: '',
+                Party: '',
+                PhoneNumber: '',
+                EmailAddress: ''
+            }
+        ]
     },
     methods: {
-        doDelete: function (id) {
+        showDelete: function (id) {
             // So with this line, we take the button-click event from the page, and grab the Id that it passed in.
             // With that Id, we can insert the name, and set the button.
+
+            // First we delete from the service, and if that works, we can just remove from the array also.
+
+            // Now to remove from the array.
             $('#delete-modal-body').html("<p>Are you sure you want to delete " + this.senators[id].Name + "?");
-            $('#deleteModal').modal();
+            $('#deleteModal').data('id', id).modal();
+        },
+        doDelete: function (id) {
+            this.senators.splice(this.senators.indexOf(this.senators[id]), 1);
+        },
+        showEdit: function (senator) {
+            this.SenatorToEdit = senator;
+            $('#editModal').modal();
+        },
+        doEdit: function () {
+            //SenatorToEdit should be here already, just need to remove/replace
+            this.senators.splice(this.senators.indexOf(this.SenatorToEdit), 1);
+            this.senators.push(SenatorToEdit);
+            this.SenatorToEdit = '';
+        },
+        showAdd: function () {
+            $('#addModal').modal();
+        },
+        doAdd: function () {
+            this.senators.push(this.SenatorToAdd);
+            this.SenatorToAdd = '';
         }
     }
-})
+});
+
+$('#btnDestroy').click(function () {
+    // handle deletion here
+    var id = $('#deleteModal').data('id');
+    //$('[data-id=' + id + ']').remove();
+    $('#deleteModal').modal('hide');
+    senatorApp.doDelete(id);
+});
+
+$('#btnUpdate').click(function () {
+    //Now we write back to the array.
+    $('#editModal').modal('hide');
+});
+
+$('#btnSave').click(function () {
+    //Now we write back to the array.
+    senatorApp.doAdd();
+    $('#addModal').modal('hide');
+});
